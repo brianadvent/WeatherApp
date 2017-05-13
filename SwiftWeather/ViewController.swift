a71fa19a25798404c849ea08e4c2063d8dbb4276
Brian Advent <brianadvent@Brians-MacBook-Pro.local>
2017-05-13 14:32:38 +0200

    Initial Commit

---
+++ SwiftWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeather
+//
+//  Created by Brian Advent on 13.05.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
