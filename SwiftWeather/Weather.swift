de33dfabeeaf6e58e155ea7b1d164b1673aadd88
Brian Advent <brianadvent@Brians-MacBook-Pro.local>
2017-05-14 09:20:51 +0200

    WeatherApp Tutorial

---
+++ SwiftWeather/Weather.swift
@@ -0,0 +1,87 @@
+//
+//  Weather.swift
+//  JSON
+//
+//  Created by Brian Advent on 11.05.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct Weather {
+    let summary:String
+    let icon:String
+    let temperature:Double
+    
+    enum SerializationError:Error {
+        case missing(String)
+        case invalid(String, Any)
+    }
+    
+    
+    init(json:[String:Any]) throws {
+        guard let summary = json["summary"] as? String else {throw SerializationError.missing("summary is missing")}
+        
+        guard let icon = json["icon"] as? String else {throw SerializationError.missing("icon is missing")}
+        
+        guard let temperature = json["temperatureMax"] as? Double else {throw SerializationError.missing("temp is missing")}
+        
+        self.summary = summary
+        self.icon = icon
+        self.temperature = temperature
+        
+    }
+    
+    
+    static let basePath = "https://api.darksky.net/forecast/YOUR_APIKEY/"
+    
+    static func forecast (withLocation location:CLLocationCoordinate2D, completion: @escaping ([Weather]?) -> ()) {
+        
+        let url = basePath + "\(location.latitude),\(location.longitude)"
+        let request = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+            
+            var forecastArray:[Weather] = []
+            
+            if let data = data {
+                
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        if let dailyForecasts = json["daily"] as? [String:Any] {
+                            if let dailyData = dailyForecasts["data"] as? [[String:Any]] {
+                                for dataPoint in dailyData {
+                                    if let weatherObject = try? Weather(json: dataPoint) {
+                                        forecastArray.append(weatherObject)
+                                    }
+                                }
+                            }
+                        }
+                    
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+                
+                completion(forecastArray)
+                
+            }
+            
+            
+        }
+        
+        task.resume()
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    }
+    
+
+}
---
+++ SwiftWeather/WeatherTableViewController.swift
@@ -0,0 +1,141 @@
+//
+//  WeatherTableViewController.swift
+//  SwiftWeather
+//
+//  Created by Brian Advent on 13.05.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherTableViewController: UITableViewController, UISearchBarDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var forecastData = [Weather]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        searchBar.delegate = self
+        
+        updateWeatherForLocation(location: "New York")
+    }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let locationString = searchBar.text, !locationString.isEmpty {
+            updateWeatherForLocation(location: locationString)
+        }
+        
+    }
+    
+    func updateWeatherForLocation (location:String) {
+        CLGeocoder().geocodeAddressString(location) { (placemarks:[CLPlacemark]?, error:Error?) in
+            if error == nil {
+                if let location = placemarks?.first?.location {
+                    Weather.forecast(withLocation: location.coordinate, completion: { (results:[Weather]?) in
+                        
+                        if let weatherData = results {
+                            self.forecastData = weatherData
+                            
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                        
+                    })
+                }
+            }
+        }
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return forecastData.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let date = Calendar.current.date(byAdding: .day, value: section, to: Date())
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        
+        return dateFormatter.string(from: date!)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let weatherObject = forecastData[indexPath.section]
+
+        cell.textLabel?.text = weatherObject.summary
+        cell.detailTextLabel?.text = "\(Int(weatherObject.temperature)) °F"
+        cell.imageView?.image = UIImage(named: weatherObject.icon)
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
